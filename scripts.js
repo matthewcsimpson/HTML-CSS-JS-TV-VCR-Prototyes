c00ffd7e435d0c237670886271069a675dde78a3
Matthew Simpson <mcsimpson+gh@gmail.com>
2024-07-07 20:37:21 -0700

    vcr added and basic functionality in place

--- scripts.js
+++ scripts.js
@@ -2,98 +2,137 @@
 const channels = [
   {
     chan: "01",
-    img: "./assets/imgs/testpattern.png",
+    img: "https://assets.codepen.io/8841541/testpattern.png",
   },
   {
     chan: "02",
-    img: "./assets/imgs/fountain.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "03",
-    img: "./assets/imgs/static.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "04",
-    img: "./assets/imgs/charlie.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "05",
-    img: "./assets/imgs/static.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "06",
-    img: "./assets/imgs/dawson.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "07",
-    img: "./assets/imgs/static.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "08",
-    img: "./assets/imgs/st.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "09",
-    img: "./assets/imgs/static.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "10",
-    img: "./assets/imgs/fellowkids.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "11",
-    img: "./assets/imgs/mal.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
   {
     chan: "12",
-    img: "./assets/imgs/homer.gif",
+    img: "https://assets.codepen.io/8841541/static.gif",
+  },
+  {
+    chan: "13",
+    img: "https://assets.codepen.io/8841541/static.gif",
   },
 ];
 
 // Selectors
+const vcrPowerButton = document.querySelector(".powerbtn .powerbutton");
+const vcrPowerButtonLight = document.querySelector(".powerbutton__light");
+const vcrTapeCarriage = document.querySelector(".carriage");
+const vcrPlayButton = document.querySelector(".playrecrewff .playbutton");
+const vcrRewButton = document.querySelector(".playrecrewff .rewbutton");
+const vcrFfButton = document.querySelector(".playrecrewff .ffbutton");
+const vcrStopButton = document.querySelector(".stoppause .stopbutton");
+const vcrPauseButton = document.querySelector(".stoppause .pausebutton");
+const vcrEjectButton = document.querySelector(".eject .ejectbutton");
+const vcrDisplayText = document.querySelector(".displayarea .displaytext");
+
 const tube = document.querySelector(".crt__image");
 const picture = document.querySelector(".crt__screen");
 const display = document.querySelector(".channel");
 
-const basePowerButton = document.querySelector(".base__button--power");
-const remotePowerbutton = document.querySelector(
-  ".remote__firstrowbutton--power"
-);
-
-const baseChannelUpButton = document.querySelector(".base__button--channelup");
-const remoteChannelUpButton = document.querySelector(
-  ".remote__functionbutton--channelup"
-);
-
-const baseChannelDownButton = document.querySelector(
-  ".base__button--channeldown"
-);
-const remoteChannelDownButton = document.querySelector(
-  ".remote__functionbutton--channeldown"
-);
-
-const numberButtons = document.querySelectorAll(".remote__number");
-const displayButton = document.querySelector(
-  ".remote__functionbutton--display"
-);
+const tvPowerButton = document.querySelector(".button--power");
+const tvChannelUpButton = document.querySelector(".button--channelup");
+const tvChannelDownButton = document.querySelector(".button--channeldown");
 
 // Functions
-const channelDisplay = (display) => {
-  display.classList.add("channel--on");
-  setTimeout(() => {
-    display.classList.remove("channel--on");
-  }, 3000);
+/**
+ * Turn the VCR on or off
+ * @param {HTMLElement} light
+ * @param {HTMLElement} display
+ */
+const vcrPowerSwitch = (light, display) => {
+  const vcrIsOn = document.querySelector(".powerbutton__light--on");
+  if (!vcrIsOn) {
+    light.classList.add("powerbutton__light--on");
+    display.classList.add("displaytext--on");
+  } else {
+    light.classList.remove("powerbutton__light--on");
+    display.classList.remove("displaytext--on");
+  }
+};
+
+/**
+ * Close the tape carriage
+ * @param {HTMLElement} carriage - The tape carriage element
+ */
+const closeCarriage = (carriage) => {
+  carriage.classList.remove("carriage--open");
+  void carriage.offsetWidth; // Trigger reflow
+  carriage.classList.add("carriage--close");
+};
+
+/**
+ * Open the tape carriage
+ * @param {HTMLElement} carriage - The tape carriage element
+ */
+const openCarriage = (carriage) => {
+  carriage.classList.remove("carriage--close");
+  void carriage.offsetWidth; // Trigger reflow
+  carriage.classList.add("carriage--open");
+};
+
+/**
+ * Toggle the tape carriage state
+ * @param {HTMLElement} carriage - The tape carriage element
+ */
+const toggleCarriage = (carriage) => {
+  if (carriage.classList.contains("carriage--close")) {
+    openCarriage(carriage);
+  } else {
+    closeCarriage(carriage);
+  }
 };
 
 /**
  * Turn the TV on or off
- * @param {Element} tube - The CRT image element
- * @param {Element} picture - The CRT screen element
- * @param {Element} display - The channel display element
+ * @param {HTMLElement} tube - The CRT image element
+ * @param {HTMLElement} picture - The CRT screen element
+ * @param {HTMLElement} display - The channel display element
+ * @param {Array} channels - Array of channel objects
  */
-const powerSwitch = (tube, picture, display, channels) => {
-  const isOn = document.querySelector(".crt__image--on");
-  if (isOn) {
+const tvPowerSwitch = (tube, picture, display, channels) => {
+  const tvIsOn = document.querySelector(".crt__image--on");
+  if (tvIsOn) {
     tube.classList.remove("crt__image--on");
     picture.classList.remove("crt__screen--on");
     display.classList.remove("channel--on");
@@ -105,14 +144,25 @@ const powerSwitch = (tube, picture, display, channels) => {
 };
 
 /**
- *
- * @param {Object} channels
- * @param {Element} tube
- * @param {Element} display
+ * Display the channel number briefly
+ * @param {HTMLElement} display - The channel display element
+ */
+const channelDisplay = (display) => {
+  display.classList.add("channel--on");
+  setTimeout(() => {
+    display.classList.remove("channel--on");
+  }, 3000);
+};
+
+/**
+ * Increment the channel
+ * @param {Array} channels - Array of channel objects
+ * @param {HTMLElement} tube - The CRT image element
+ * @param {HTMLElement} display - The channel display element
  */
 const channelUp = (channels, tube, display) => {
-  const isOn = document.querySelector(".crt__image--on");
-  if (isOn) {
+  const tvIsOn = document.querySelector(".crt__image--on");
+  if (tvIsOn) {
     const currentChannel = display.innerText;
     const currentChannelIndex = channels.findIndex(
       (channel) => channel.chan === currentChannel
@@ -126,20 +176,18 @@ const channelUp = (channels, tube, display) => {
 };
 
 /**
- *
- * @param {Object} channels
- * @param {Element} tube
- * @param {Element} display
+ * Decrement the channel
+ * @param {Array} channels - Array of channel objects
+ * @param {HTMLElement} tube - The CRT image element
+ * @param {HTMLElement} display - The channel display element
  */
 const channelDown = (channels, tube, display) => {
-  const isOn = document.querySelector(".crt__image--on");
-
-  if (isOn) {
+  const tvIsOn = document.querySelector(".crt__image--on");
+  if (tvIsOn) {
     const currentChannel = display.innerText;
     const currentChannelIndex = channels.findIndex(
       (channel) => channel.chan === currentChannel
     );
-
     const nextChannelIndex =
       currentChannelIndex <= 0 ? channels.length - 1 : currentChannelIndex - 1;
     display.innerText = channels[nextChannelIndex].chan;
@@ -148,71 +196,36 @@ const channelDown = (channels, tube, display) => {
   }
 };
 
-/**
- * Select a channel directly.
- * If the channel does not exist, display the channel number + static
- * @param {Array} channels
- * @param {Element} tube
- * @param {Element} display
- * @param {Array} channelInput
- */
-const channelSelect = (channels, tube, display, input) => {
-  const channel = channels.find((channel) => channel.chan === input);
-  if (channel) {
-    display.innerText = channel.chan;
-    tube.style.backgroundImage = `url(${channel.img})`;
-    channelDisplay(display);
-  } else {
-    display.innerText = input;
-    tube.style.backgroundImage = `url(./assets/imgs/static.gif)`;
-    channelDisplay(display);
-  }
-};
-
 // Default
-const channelInput = [];
 display.innerText = channels[0].chan;
 tube.style.backgroundImage = `url(${channels[0].img})`;
 
 // Event Listeners
-// Power
-basePowerButton.addEventListener("click", () => {
-  powerSwitch(tube, picture, display, channels);
+// VCR Power
+vcrPowerButton.addEventListener("click", () => {
+  vcrPowerSwitch(vcrPowerButtonLight, vcrDisplayText);
 });
-remotePowerbutton.addEventListener("click", () => {
-  powerSwitch(tube, picture, display, channels);
+
+// VCR Carriage Open or Close
+vcrTapeCarriage.addEventListener("click", () => {
+  closeCarriage(vcrTapeCarriage);
+});
+
+vcrEjectButton.addEventListener("click", () => {
+  openCarriage(vcrTapeCarriage);
+});
+
+// TV Power
+tvPowerButton.addEventListener("click", () => {
+  tvPowerSwitch(tube, picture, display, channels);
 });
 
 // Channel Up
-baseChannelUpButton.addEventListener("click", () => {
-  channelUp(channels, tube, display);
-});
-remoteChannelUpButton.addEventListener("click", () => {
+tvChannelUpButton.addEventListener("click", () => {
   channelUp(channels, tube, display);
 });
 
 // Channel Down
-baseChannelDownButton.addEventListener("click", () => {
+tvChannelDownButton.addEventListener("click", () => {
   channelDown(channels, tube, display);
-});
-remoteChannelDownButton.addEventListener("click", () => {
-  channelDown(channels, tube, display);
-});
-
-// Number Buttons
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const isOn = document.querySelector(".crt__image--on");
-    if (isOn) {
-      channelInput.push(button.innerText);
-      if (channelInput.length === 2) {
-        channelSelect(channels, tube, display, channelInput.join(""));
-        channelInput.length = 0;
-      }
-    }
-  });
-});
-
-displayButton.addEventListener("click", () => {
-  channelDisplay(display);
 });
