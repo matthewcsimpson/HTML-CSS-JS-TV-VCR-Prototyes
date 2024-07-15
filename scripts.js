25103427b091d08709ca23451e71fab58665559d
Matthew Simpson <mcsimpson+gh@gmail.com>
2024-07-15 03:17:33 -0700

    volume working.  stored playback time working.

--- scripts.js
+++ scripts.js
@@ -10,23 +10,36 @@ const vcrPauseButton = document.querySelector(".stoppause .pausebutton");
 const vcrEjectButton = document.querySelector(".eject .ejectbutton");
 const vcrDisplayText = document.querySelector(".displayarea .displaytext");
 
-const crtPicture = document.querySelector(".crt__image");
-const crtGlass = document.querySelector(".crt__glass");
-const channelNum = document.querySelector(".channel");
-
 const tvPowerButton = document.querySelector(".button--power");
 const tvChannelUpButton = document.querySelector(".button--channelup");
 const tvChannelDownButton = document.querySelector(".button--channeldown");
+const tvVolumeUpButton = document.querySelector(".button--volumeup");
+const tvVolumeDownButton = document.querySelector(".button--volumedown");
+const tvPictureBackground = document.querySelector(".crt__background");
+
+const tvWrongChannel = document.querySelector(".crt__image--wrongchannel");
+const tvChannelThree = document.querySelector(".crt__image--channelthree");
+const tvGlass = document.querySelector(".crt__glass");
+
+const displayChannelNum = document.querySelector(".channel");
+const displayIndicator = document.querySelector(".indicator");
+const displayPlay = document.querySelector(".indicator__icon--play");
+const displayPause = document.querySelector(".indicator__icon--pause");
+const displayStop = document.querySelector(".indicator__icon--stop");
+const displayRew = document.querySelector(".indicator__icon--rewind");
+const displayFf = document.querySelector(".indicator__icon--fastforward");
 
 // Constants & Variables
 const videoURL =
   "https://ia800909.us.archive.org/32/items/Detour/Detour_512kb.mp4";
 const staticImageURL = "https://assets.codepen.io/8841541/static.gif";
+const NUMBER_OF_CHANNELS = 13;
+const VOLUME_LEVELS = 10;
+
 let rewindInterval = null;
 let movieTime = 0;
 let rewindSpeed = 1;
-
-// Helper Functions
+let currentVolume = 1;
 
 /**
  * Adds a class to an element.
@@ -47,48 +60,59 @@ const removeClass = (element, className) => {
 };
 
 /**
- * Updates the CRT picture.
- * @param {string} img - The image URL.
- * @param {string} video - The video URL.
- */
-const updateCRTPicture = (img, video) => {
-  crtPicture.setAttribute("poster", img || "");
-  crtPicture.setAttribute("src", video || "");
-};
-
-// Main Functions
-
-/**
  * Gets the current power status of the TV and VCR
  * @returns powerStatus - The current power status of the TV and VCR
  */
 const getCurrentStatus = () => {
   return {
-    tvIsOn: document.querySelector(".crt__image--on"),
+    tvIsOn: document.querySelector(".crt__glass--on"),
     vcrIsOn: document.querySelector(".powerbutton__light--on"),
     carriageIsClosed: document.querySelector(".carriage--close"),
   };
 };
 
+// Main Functions
+
 /**
- * Update the channel and CRT image based on the channel number.
+ * Update the channel and TV image based on the channel number.
  */
 const updateWhatsOn = () => {
-  const { vcrIsOn, carriageIsClosed } = getCurrentStatus();
-  const currentChannel = parseInt(channelNum.innerText.trim());
+  const { tvIsOn, vcrIsOn, carriageIsClosed } = getCurrentStatus();
+  const currentChannel = parseInt(displayChannelNum.innerText.trim());
 
-  if (currentChannel === 3) {
-    if (vcrIsOn) {
-      if (carriageIsClosed) {
-        updateCRTPicture("", videoURL); // Play the video
+  if (tvIsOn) {
+    if (currentChannel === 3) {
+      if (vcrIsOn) {
+        if (carriageIsClosed) {
+          console.log("---> tvOn, channe3, vcrOn, carriageClosed");
+
+          removeClass(tvChannelThree, "crt__image--hidden");
+          addClass(tvWrongChannel, "crt__image--hidden");
+          tvChannelThree.volume = currentVolume;
+        } else {
+          console.log("----> tvOn, channel 3, vcrOn, carriageOpen");
+          addClass(tvChannelThree, "crt__image--hidden");
+          removeClass(tvWrongChannel, "crt__image--hidden");
+          addClass(tvWrongChannel, "crt__image--on");
+          removeClass(tvWrongChannel, "crt__image--static");
+        }
       } else {
-        updateCRTPicture("", ""); // Show the blue screen
+        console.log("----> tvOn, channel 3, vcrOFF, carriageOpen");
+
+        addClass(tvChannelThree, "crt__image--hidden");
+        removeClass(tvWrongChannel, "crt__image--hidden");
+        removeClass(tvWrongChannel, "crt__image--on");
+        addClass(tvWrongChannel, "crt__image--static");
+        tvChannelThree.volume = 0;
       }
     } else {
-      updateCRTPicture(staticImageURL, ""); // Show static when VCR is off
+      console.log("----> tvOn, channel not 3");
+      addClass(tvChannelThree, "crt__image--hidden");
+      removeClass(tvWrongChannel, "crt__image--hidden");
+      removeClass(tvWrongChannel, "crt__image--on");
+      addClass(tvWrongChannel, "crt__image--static");
+      tvChannelThree.volume = 0;
     }
-  } else {
-    updateCRTPicture(staticImageURL, ""); // Show static for other channels
   }
 };
 
@@ -97,10 +121,12 @@ const updateWhatsOn = () => {
  */
 const vcrPowerSwitch = () => {
   const { vcrIsOn } = getCurrentStatus();
+
   if (!vcrIsOn) {
     addClass(vcrPowerButtonLight, "powerbutton__light--on");
     addClass(vcrDisplayText, "displaytext--on");
   } else {
+    tvChannelThree.pause();
     removeClass(vcrPowerButtonLight, "powerbutton__light--on");
     removeClass(vcrDisplayText, "displaytext--on");
   }
@@ -111,11 +137,11 @@ const vcrPowerSwitch = () => {
  * Start rewinding the video at 2x speed
  */
 const startRewind = () => {
-  crtPicture.pause();
+  tvChannelThree.pause();
   clearInterval(rewindInterval);
   rewindInterval = setInterval(() => {
-    crtPicture.currentTime = Math.max(
-      crtPicture.currentTime - 0.1 * rewindSpeed,
+    tvChannelThree.currentTime = Math.max(
+      tvChannelThree.currentTime - 0.1 * rewindSpeed,
       0
     );
   }, 50);
@@ -152,9 +178,14 @@ const formatTime = (timer) => {
  * Close the tape carriage
  */
 const closeCarriage = () => {
+  console.log("close movietime", movieTime);
+
   removeClass(vcrTapeCarriage, "carriage--open");
   void vcrTapeCarriage.offsetWidth; // Force reflow
   addClass(vcrTapeCarriage, "carriage--close");
+  tvChannelThree.setAttribute("src", videoURL);
+  tvChannelThree.currentTime = movieTime;
+  tvChannelThree.load();
   updateWhatsOn();
 };
 
@@ -164,9 +195,18 @@ const closeCarriage = () => {
 const ejectCarriage = () => {
   const { vcrIsOn } = getCurrentStatus();
   if (vcrIsOn) {
+    movieTime = tvChannelThree.currentTime;
+    console.log("eject movietime", movieTime);
     removeClass(vcrTapeCarriage, "carriage--close");
+    console.log("eject movietime", movieTime);
     void vcrTapeCarriage.offsetWidth; // Force reflow
+    console.log("eject movietime", movieTime);
+    tvChannelThree.setAttribute("src", "");
+    console.log("eject movietime", movieTime);
+    tvChannelThree.load();
+    console.log("eject movietime", movieTime);
     addClass(vcrTapeCarriage, "carriage--open");
+    console.log("eject movietime", movieTime);
     updateWhatsOn();
   }
 };
@@ -176,14 +216,15 @@ const ejectCarriage = () => {
  */
 const tvPowerSwitch = () => {
   const { tvIsOn } = getCurrentStatus();
+
   if (tvIsOn) {
-    removeClass(crtPicture, "crt__image--on");
-    removeClass(crtGlass, "crt__glass--on");
-    removeClass(channelNum, "channel--on");
+    removeClass(tvGlass, "crt__glass--on");
+    removeClass(displayChannelNum, "channel--on");
+    addClass(tvWrongChannel, "crt__image--hidden");
+    addClass(tvChannelThree, "crt__image--hidden");
   } else {
+    addClass(tvGlass, "crt__glass--on");
     updateWhatsOn();
-    addClass(crtPicture, "crt__image--on");
-    addClass(crtGlass, "crt__glass--on");
     channelDisplay();
   }
 };
@@ -192,11 +233,12 @@ const tvPowerSwitch = () => {
  * Display the channel number briefly
  */
 const channelDisplay = () => {
-  addClass(channelNum, "channel--on");
+  addClass(displayChannelNum, "channel--on");
   setTimeout(() => {
-    removeClass(channelNum, "channel--on");
+    removeClass(displayChannelNum, "channel--on");
   }, 3000);
 };
+
 
 /**
  * Increment the channel
@@ -204,9 +246,9 @@ const channelDisplay = () => {
 const channelUp = () => {
   const { tvIsOn } = getCurrentStatus();
   if (tvIsOn) {
-    let currentChannel = parseInt(channelNum.innerText.trim());
-    currentChannel = (currentChannel % 13) + 1;
-    channelNum.innerText = String(currentChannel).padStart(2, "0");
+    let currentChannel = parseInt(displayChannelNum.innerText.trim());
+    currentChannel = (currentChannel % NUMBER_OF_CHANNELS) + 1;
+    displayChannelNum.innerText = String(currentChannel).padStart(2, "0");
     updateWhatsOn();
     channelDisplay();
   }
@@ -218,45 +260,34 @@ const channelUp = () => {
 const channelDown = () => {
   const { tvIsOn } = getCurrentStatus();
   if (tvIsOn) {
-    let currentChannel = parseInt(channelNum.innerText.trim());
-    currentChannel = ((currentChannel - 2 + 13) % 13) + 1;
-    channelNum.innerText = String(currentChannel).padStart(2, "0");
+    let currentChannel = parseInt(displayChannelNum.innerText.trim());
+    currentChannel =
+      ((currentChannel - 2 + NUMBER_OF_CHANNELS) % NUMBER_OF_CHANNELS) + 1;
+    displayChannelNum.innerText = String(currentChannel).padStart(2, "0");
     updateWhatsOn();
     channelDisplay();
   }
 };
 
 // Event Listeners
+// VCR
 // VCR Power
 vcrPowerButton.addEventListener("click", vcrPowerSwitch);
 
 // VCR Carriage Open or Close
 vcrTapeCarriage.addEventListener("click", closeCarriage);
-
 vcrEjectButton.addEventListener("click", ejectCarriage);
-
-// TV Power
-tvPowerButton.addEventListener("click", tvPowerSwitch);
-
-// Channel Up
-tvChannelUpButton.addEventListener("click", channelUp);
-
-// Channel Down
-tvChannelDownButton.addEventListener("click", channelDown);
-
-// Track the time of the video
-crtPicture.addEventListener("timeupdate", () => {
-  movieTime = formatTime(crtPicture.currentTime);
-  vcrDisplayText.innerText = movieTime;
-});
 
 // Play Button
 vcrPlayButton.addEventListener("click", () => {
-  if (crtPicture.getAttribute("src") === videoURL) {
-    crtPicture.play();
-
-    if (crtPicture.playbackRate !== 1) {
-      crtPicture.playbackRate = 1;
+  const { tvIsOn, vcrIsOn } = getCurrentStatus();
+  if (tvChannelThree.getAttribute("src") === videoURL && vcrIsOn) {
+    tvChannelThree.play();
+    if (!tvIsOn) {
+      tvChannelThree.volume = 0;
+    }
+    if (tvChannelThree.playbackRate !== 1) {
+      tvChannelThree.playbackRate = 1;
     }
     if (rewindInterval) {
       stopRewind();
@@ -266,28 +297,29 @@ vcrPlayButton.addEventListener("click", () => {
 
 // Pause Button
 vcrPauseButton.addEventListener("click", () => {
-  if (crtPicture.getAttribute("src") === videoURL) {
-    crtPicture.pause();
+  if (tvChannelThree.getAttribute("src") === videoURL) {
+    tvChannelThree.pause();
   }
 });
 
 // Fast Forward Button
 vcrFfButton.addEventListener("click", () => {
-  if (crtPicture.getAttribute("src") === videoURL) {
-    if (crtPicture.paused || rewindInterval) {
-      crtPicture.playbackRate = 1;
-      crtPicture.play();
+  const { vcrIsOn } = getCurrentStatus();
+  if (tvChannelThree.getAttribute("src") === videoURL && vcrIsOn) {
+    if (tvChannelThree.paused || rewindInterval) {
+      tvChannelThree.playbackRate = 1;
+      tvChannelThree.play();
       if (rewindInterval) {
         stopRewind();
       }
     }
 
-    switch (crtPicture.playbackRate) {
+    switch (tvChannelThree.playbackRate) {
       case 1:
-        crtPicture.playbackRate = 2;
+        tvChannelThree.playbackRate = 2;
         break;
       case 2:
-        crtPicture.playbackRate = 4;
+        tvChannelThree.playbackRate = 4;
         break;
       default:
         // Do nothing for any other playbackRate
@@ -298,7 +330,7 @@ vcrFfButton.addEventListener("click", () => {
 
 // Rewind Button
 vcrRewButton.addEventListener("click", () => {
-  if (crtPicture.getAttribute("src") === videoURL) {
+  if (tvChannelThree.getAttribute("src") === videoURL) {
     if (rewindInterval) {
       switch (rewindSpeed) {
         case 1:
@@ -322,9 +354,34 @@ vcrRewButton.addEventListener("click", () => {
 
 // Stop Button
 vcrStopButton.addEventListener("click", () => {
-  if (crtPicture.getAttribute("src") === videoURL) {
-    crtPicture.pause();
-    crtPicture.removeAttribute("src"); // Remove the video source
-    crtPicture.load(); // Reset the video element
+  if (tvChannelThree.getAttribute("src") === videoURL) {
+    tvChannelThree.pause();
+  }
+});
+
+// Track the time of the video
+tvChannelThree.addEventListener("timeupdate", () => {
+  vcrDisplayText.innerText = formatTime(tvChannelThree.currentTime);
+});
+
+// TV
+// TV Power
+tvPowerButton.addEventListener("click", tvPowerSwitch);
+// Change Channel
+tvChannelUpButton.addEventListener("click", channelUp);
+tvChannelDownButton.addEventListener("click", channelDown);
+// Change Volume
+tvVolumeUpButton.addEventListener("click", () => {
+  const volume = tvChannelThree.volume;
+  if (volume < 1) {
+    tvChannelThree.volume += 1 / VOLUME_LEVELS;
+    currentVolume = tvChannelThree.volume;
+  }
+});
+tvVolumeDownButton.addEventListener("click", () => {
+  const volume = tvChannelThree.volume;
+  if (volume > 0) {
+    tvChannelThree.volume -= 1 / VOLUME_LEVELS;
+    currentVolume = tvChannelThree.volume;
   }
 });
